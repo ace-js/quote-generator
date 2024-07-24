f6ffd580176ae9da1a04997f7358faaaf30269a4
Arnaud Bertieaux <30495316+ace-js@users.noreply.github.com>
2024-07-25 00:13:19 +0200

    Update quotesService.ts

--- backend/src/services/quotesService.ts
+++ backend/src/services/quotesService.ts
@@ -2,7 +2,8 @@ import apiClient from '../utils/apiClient';
 import { Quote } from '../models/quote';
 import logger from '../utils/logger';
 import { GetQuotesParams } from '../requests/getQuotesParms';
-
+// improvment: using ioc container to rely on interfaces for all dependencies and services
+// that would make code base more resilient to changes and more scalable. (solution can be based on nestjs or inversify)
 export const getRandomQuotes = async (params: GetQuotesParams): Promise<Quote[]> => {
   try {
     const response = await apiClient.get(`${process.env.API_BASE_URL}/quotes/random`, { params });
