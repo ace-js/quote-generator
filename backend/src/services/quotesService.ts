5767014dd4d8a7c8f89cf2b9d6c6f0ab086e2883
Arnaud Bertieaux <arnaud@Arnauds-MacBook-Pro.local>
2024-07-25 07:51:07 +0200

    add type to get call

--- backend/src/services/quotesService.ts
+++ backend/src/services/quotesService.ts
@@ -3,10 +3,11 @@ import { Quote } from '../models/quote';
 import logger from '../utils/logger';
 import { GetQuotesParams } from '../requests/getQuotesParms';
 // improvment: using ioc container to rely on interfaces for all dependencies and services
-// that would make code base more resilient to changes and more scalable. (solution can be based on nestjs or inversify)
+// that would make the codebase more resilient to changes and more scalable. 
+// potential solution can be based on nestjs or inversify
 export const getRandomQuotes = async (params: GetQuotesParams): Promise<Quote[]> => {
   try {
-    const response = await apiClient.get(`${process.env.API_BASE_URL}/quotes/random`, { params });
+    const response = await apiClient.get<Quote[]>(`${process.env.API_BASE_URL}/quotes/random`, { params });
     return response.data;
   } catch (error) {
     logger.error('Failed to fetch quotes', { error });
